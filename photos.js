fcc1f6ccb5bf3921a9927ad33f9e3e7493339af9
Marcel Cornett <marcornett@gmail.com>
2020-04-17 14:37:39 -0400

    Still trying to figure out how to display one image at a time with a button.

--- photos.js
+++ photos.js
@@ -1,23 +1,78 @@
+let userInput = 'ducks'
 
-userLocation (location) => {displayImage(location)}
+userLocation = (location) => { grabDataPromise(location.coords) }
+const fallBackLocation = { latitude: 32.715736, longitude: -117.161087 }
+useFallBackLocation = (err) => { grabDataPromise(fallBackLocation) }
+const options = { maximumAge: 0, enableHighAccuracy: true }
 
-navigator.geolocation.getCurrentPosition(userLocation, fallBackLocation, option)
+navigator.geolocation.getCurrentPosition(userLocation, useFallBackLocation, options)
 
-fetch('https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=93bdea8ccea36d596c0021e136e28669&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=39.76574&lon=-86.1579024&text=ducks')
-.then(response => response.json())
-.then(function (dataObj){
-    console.log(dataObj)
-    let photosArr = dataObj.photos.photo
-    
-    photosArr.forEach(function (photoObj){
-        let photoTitle = photoObj.title
-        let header = document.createElement('h3')
-        let image = document.createElement('img')
 
-        image.setAttribute('src',"https://farm" + photoObj.farm + ".staticflickr.com/" + photoObj.server + "/" + photoObj.id + "_" + photoObj.secret + ".jpg")
-        header.appendChild(document.createTextNode(photoTitle))
-        document.body.appendChild(header)
+function userSearchByText() {
+    let submitButton = document.querySelector('#submit')
+    submitButton.addEventListener('click', function (event) {
+        userInput = document.querySelector('#user-input').value
+        // hard coded fallback location, need to find alternative
+        grabDataPromise(fallBackLocation, userInput)
+    })
+}
+userSearchByText()
+
+function constructImageURL(photoObj) {
+    const { farm, server, id, secret } = photoObj
+    return "https://farm" + farm +
+        ".staticflickr.com/" + server +
+        "/" + id + "_" + secret + ".jpg";
+}
+
+function grabDataPromise(location, userInputSearch = userInput) {
+    let responsePromise = fetch(`https://shrouded-mountain-15003.herokuapp.com/` +
+        `https://flickr.com/services/rest/` +
+        `?api_key=93bdea8ccea36d596c0021e136e28669&` +
+        `format=json&` +
+        `nojsoncallback=1&` +
+        `method=flickr.photos.search&` +
+        `safe_search=1&` +
+        `per_page=5&` +
+        `lat=${location.latitude}&` +
+        `lon=${location.longitude}&` +
+        `text=${userInputSearch}`)
+
+    let dataPromise = responsePromise.then(response => response.json())
+    dataPromise.then(function (dataObj) {
+        let photosObj = dataObj.photos.photo
+        displayImage(photosObj)
+    })
+}
+
+let photosArr = []
+function addPhotoObjToArr(obj) {
+    photosArr.push(obj)
+    nextImageSelection(photosArr)
+}
+
+
+function displayImage(photosObj) {
+    let photoUrl = 'Link to image'
+    let link = document.createElement('a')
+    let image = document.createElement('img')
+    addPhotoObjToArr(photosObj)
+    photosObj.forEach(function (photo) {
+        const url = constructImageURL(photo)
+        image.setAttribute('src', url)
+        // Find a link to the flickr image page instead of just the image
+        link.setAttribute('href', url)
+        link.appendChild(document.createTextNode(photoUrl))
+        document.body.appendChild(link)
         document.body.appendChild(image)
     })
-})
+}
 
+function nextImageSelection(arr) {
+    let nextPhotoButton = document.querySelector('#next')
+    nextPhotoButton.addEventListener('click', function (event) {
+        arr.forEach(function(photo){
+            console.log(photo)
+        })
+    })
+}
